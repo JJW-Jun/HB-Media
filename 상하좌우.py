aa7eeb39e3832c40028ada86ef4ac7b41fef4f3e
JJW-Jun <68919333+JJW-Jun@users.noreply.github.com>
2020-09-26 23:45:33 +0900

    Update 상하좌우.py

--- 상하좌우.py
+++ 상하좌우.py
@@ -1,3 +1,4 @@
+# Solution 01 : Greedy
 def location(number) :
     a, b = 1, 1
     for i in range(len(number)) :
@@ -27,3 +28,30 @@ def location(number) :
     return a, b
 
 print(location(["R", "R", "R", "R", "D", "D"]) )
+
+
+
+
+# Solution 02 : 
+n, plans = int(input()), input().split()
+(x, y) = (1, 1)
+
+# L, R, U, D
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move = ['L', 'R', 'U', 'D']
+
+for plan in plans :
+    print('plan값 확인 : ', plan)
+    for i in range(len(move)) :
+
+        print('i값 확인 : ', i)
+        if plan == move[i] :
+            nx = x + dx[i]
+            ny = y + dy[i]
+            print('nx, ny값 확인 : ', (nx, ny))
+    if nx < 1 or ny < 1 or nx > n or ny > n :
+        print(nx, ny, '오류 발생')
+        continue
+
+    x, y = nx, ny
