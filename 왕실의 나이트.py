1c5613397f285403dd7720b184b28a7035eb57e1
JJW-Jun <68919333+JJW-Jun@users.noreply.github.com>
2020-09-28 04:26:32 +0900

    Update 왕실의 나이트.py

--- 왕실의 나이트.py
+++ 왕실의 나이트.py
@@ -2,8 +2,10 @@
 input_data = input()
 row, column = int(input_data[1]), int(ord(input_data[0])-int(ord('a')+1))
 
+
 # 말이 이동할 수 있는 8가지 방향의 좌표값을 나타낸 리스트.
 steps = [(-2, -1), (1, -2), (2, -1), (2,1), (1, 2), (-1, 2), (-2, 1)]
+
 
 # if문을 활용해 말이 움직일 수 있는지 체크.
 count = 0
