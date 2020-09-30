97dc3b7621c75edb39cebd9ac80bcb145f013c46
Jun___ <68919333+JJW-Jun@users.noreply.github.com>
2020-09-30 22:12:23 +0900

    Source

---
+++ DFS.py
@@ -0,0 +1,14 @@
+def dfs(graph, v, visited) :
+    visited[v] = True
+    print(v, 'start', end= ' ')
+
+    for i in graph[v] :
+        print(i, graph[v], 'i값')
+        if not visited[i] :
+            print(visited)
+            dfs(graph, i, visited)
+
+graph = [[], [2, 3, 8], [1, 7], [1, 4, 5], [3, 5], [3, 4], [7], [2, 6, 8], [1, 7]]
+visited = [False] * len(graph)
+
+print(dfs(graph, 1, visited))
